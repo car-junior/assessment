ffd38a301327befa0108626f2c732396a7030165
car-junior <carlosilva.dev@gmail.com>
2024-04-21 22:37:04 -0300

    test(item-integration): Adiciona teste de integração para verificar a funcionalidade de busca de todos items no sistema sem filtros aplicados

--- src/test/java/com/senior/assessment/integration/ItemControllerIntegrationTest.java
+++ src/test/java/com/senior/assessment/integration/ItemControllerIntegrationTest.java
@@ -1,11 +1,13 @@
 package com.senior.assessment.integration;
 
 import com.senior.assessment.domain.config.AssessmentIntegrationConfig;
+import com.senior.assessment.domain.dto.PageResult;
 import com.senior.assessment.domain.dto.item.ItemCreateUpdateDto;
 import com.senior.assessment.domain.dto.item.ItemDetailDto;
 import com.senior.assessment.domain.enums.ItemStatus;
 import com.senior.assessment.domain.enums.ItemType;
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.common.mapper.TypeRef;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
@@ -127,5 +129,49 @@ public class ItemControllerIntegrationTest extends AssessmentIntegrationConfig {
         assertEquals(itemCreateUpdateDto.getType(), itemDetailDto.getType());
         assertEquals(itemCreateUpdateDto.getStatus(), itemDetailDto.getStatus());
         assertThat(itemDetailDto.getPrice()).isEqualByComparingTo(itemCreateUpdateDto.getPrice());
+    }
+
+    @Test
+    @Order(4)
+    void testGivenEmptyItemSearchAndPaginationDefault_whenGetAllItem_thenReturnPageResultItemDetailDto() {
+        var newItemCreateDto = ItemCreateUpdateDto.builder()
+                .name("Teclado")
+                .type(ItemType.PRODUCT)
+                .status(ItemStatus.ACTIVE)
+                .price(BigDecimal.valueOf(100.00))
+                .build();
+
+        given().spec(requestSpecification)
+                .contentType(CONTENT_TYPE_JSON)
+                .body(newItemCreateDto)
+                .when()
+                .post()
+                .then()
+                .statusCode(200);
+
+        PageResult<ItemDetailDto> pageResult = given()
+                .spec(requestSpecification)
+                .when()
+                .get()
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .as(new TypeRef<>() {
+                });
+
+        assertNotNull(pageResult);
+        assertNotNull(pageResult.getResult());
+        assertEquals(1, pageResult.getTotalPages());
+        assertEquals(2, pageResult.getTotalResults());
+        pageResult.getResult()
+                .forEach(itemDetailDto -> {
+                    assertNotNull(itemDetailDto.getId());
+                    assertNotNull(itemDetailDto.getName());
+                    assertNotNull(itemDetailDto.getType());
+                    assertNotNull(itemDetailDto.getStatus());
+                    assertNotNull(itemDetailDto.getCreatedDate());
+                    assertNotNull(itemDetailDto.getLastModifiedDate());
+                });
     }
 }
