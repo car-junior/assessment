e2d2c8a5c23a42de98f5ec05509b284b69dcaf13
car-junior <carlosilva.dev@gmail.com>
2024-04-21 21:57:10 -0300

    test(item-integration): Adiciona testes de integração para criação, edição e retorno de item caso positivo

---
+++ src/test/java/com/senior/assessment/integration/ItemControllerIntegrationTest.java
@@ -0,0 +1,131 @@
+package com.senior.assessment.integration;
+
+import com.senior.assessment.domain.config.AssessmentIntegrationConfig;
+import com.senior.assessment.domain.dto.item.ItemCreateUpdateDto;
+import com.senior.assessment.domain.dto.item.ItemDetailDto;
+import com.senior.assessment.domain.enums.ItemStatus;
+import com.senior.assessment.domain.enums.ItemType;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import static com.senior.assessment.domain.config.ServerConfigTest.CONTENT_TYPE_JSON;
+import static com.senior.assessment.domain.config.ServerConfigTest.SERVER_PORT;
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class ItemControllerIntegrationTest extends AssessmentIntegrationConfig {
+    private static UUID itemId;
+    private static ItemCreateUpdateDto itemCreateUpdateDto;
+    private static RequestSpecification requestSpecification;
+
+    @BeforeAll
+    public static void setup() {
+        requestSpecification = new RequestSpecBuilder()
+                .setBasePath("/items")
+                .setPort(SERVER_PORT)
+                .addFilter(new RequestLoggingFilter(LogDetail.ALL))
+                .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
+                .build();
+
+        itemCreateUpdateDto = ItemCreateUpdateDto.builder()
+                .name("Smartphone XYZ")
+                .type(ItemType.PRODUCT)
+                .status(ItemStatus.ACTIVE)
+                .price(BigDecimal.valueOf(799.99))
+                .build();
+    }
+
+    @Test
+    @Order(1)
+    void testGivenItemCreateUpdateDto_whenCreateItem_thenReturn200AndItemDetailDto() {
+        var itemDetailDto = given()
+                .spec(requestSpecification)
+                .contentType(CONTENT_TYPE_JSON)
+                .body(itemCreateUpdateDto)
+                .when()
+                .post()
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .as(ItemDetailDto.class);
+
+        itemId = itemDetailDto.getId();
+
+        assertNotNull(itemDetailDto);
+        assertNotNull(itemDetailDto.getId());
+        assertNotNull(itemDetailDto.getCreatedDate());
+        assertNotNull(itemDetailDto.getLastModifiedDate());
+        assertEquals(ItemStatus.ACTIVE, itemDetailDto.getStatus());
+        assertEquals(itemCreateUpdateDto.getName(), itemDetailDto.getName());
+        assertEquals(itemCreateUpdateDto.getType(), itemDetailDto.getType());
+        assertThat(itemDetailDto.getPrice()).isEqualByComparingTo(itemCreateUpdateDto.getPrice());
+    }
+
+    @Test
+    @Order(2)
+    void testGivenItemCreateUpdateDto_whenUpdateItem_thenReturn200AndItemDetailDto() {
+        itemCreateUpdateDto.setName("Manutenção Residencial");
+        itemCreateUpdateDto.setType(ItemType.SERVICE);
+        itemCreateUpdateDto.setStatus(ItemStatus.DISABLED);
+        itemCreateUpdateDto.setPrice(BigDecimal.valueOf(1000.00));
+
+        var itemDetailDto = given()
+                .spec(requestSpecification)
+                .contentType(CONTENT_TYPE_JSON)
+                .body(itemCreateUpdateDto)
+                .when()
+                .put("/{itemId}", itemId)
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .as(ItemDetailDto.class);
+
+        assertNotNull(itemDetailDto);
+        assertNotNull(itemDetailDto.getId());
+        assertNotNull(itemDetailDto.getCreatedDate());
+        assertNotNull(itemDetailDto.getLastModifiedDate());
+        assertEquals(itemId, itemDetailDto.getId());
+        assertEquals(itemCreateUpdateDto.getName(), itemDetailDto.getName());
+        assertEquals(itemCreateUpdateDto.getType(), itemDetailDto.getType());
+        assertEquals(itemCreateUpdateDto.getStatus(), itemDetailDto.getStatus());
+        assertThat(itemDetailDto.getPrice()).isEqualByComparingTo(itemCreateUpdateDto.getPrice());
+    }
+
+    @Test
+    @Order(3)
+    void testGivenItemId_whenGetItemById_thenReturn200AndItemDetailDto() {
+        var itemDetailDto = given()
+                .spec(requestSpecification)
+                .when()
+                .get("/{categoryId}", itemId)
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .as(ItemDetailDto.class);
+
+        assertNotNull(itemDetailDto);
+        assertNotNull(itemDetailDto.getId());
+        assertNotNull(itemDetailDto.getCreatedDate());
+        assertNotNull(itemDetailDto.getLastModifiedDate());
+        assertEquals(itemId, itemDetailDto.getId());
+        assertEquals(itemCreateUpdateDto.getName(), itemDetailDto.getName());
+        assertEquals(itemCreateUpdateDto.getType(), itemDetailDto.getType());
+        assertEquals(itemCreateUpdateDto.getStatus(), itemDetailDto.getStatus());
+        assertThat(itemDetailDto.getPrice()).isEqualByComparingTo(itemCreateUpdateDto.getPrice());
+    }
+}
