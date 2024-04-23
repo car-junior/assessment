8263bde2d135b18bffb632df8b45403b533bdbdd
car-junior <carlosilva.dev@gmail.com>
2024-04-23 14:55:45 -0300

    feat: Ajusta mensagens de documentação usando o swagger

--- src/main/java/com/senior/assessment/rest/ItemController.java
+++ src/main/java/com/senior/assessment/rest/ItemController.java
@@ -9,6 +9,8 @@ import com.senior.assessment.domain.enums.ItemStatus;
 import com.senior.assessment.domain.enums.ItemType;
 import com.senior.assessment.domain.querydsl.search.ItemSearch;
 import com.senior.assessment.domain.service.ItemService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -21,16 +23,24 @@ import static com.senior.assessment.utilities.Utils.createPagination;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("items")
+@Tag(name = "Itens",
+        description = "Contém as operações para Cadastrar, Listar, Editar e Excluir Items(Produto/Serviço)"
+)
 public class ItemController {
     private final ItemService itemService;
     private final ModelMapperService modelMapperService;
 
+    @Operation(summary = "Operação para criar um item(Serviço/Produto).",
+            description = "Olhe o schema ItemCreateUpdateDto para verificar as regras de cadastro."
+    )
     @PostMapping
     public ResponseEntity<ItemDetailDto> create(@Valid @RequestBody ItemCreateUpdateDto itemCreate) {
         var item = itemService.createItem(modelMapperService.toObject(Item.class, itemCreate));
         return ResponseEntity.ok(modelMapperService.toObject(ItemDetailDto.class, item));
     }
 
+    @Operation(summary = "Operação para atualizar um item(Serviço/Produto) por meio do ID.",
+            description = "Olhe o schema ItemCreateUpdateDto para verificar as regras de cadastro.")
     @PutMapping("/{itemId}")
     public ResponseEntity<ItemDetailDto> update(@PathVariable(name = "itemId") UUID itemId,
                                                 @Valid @RequestBody ItemCreateUpdateDto itemUpdate) {
@@ -39,7 +49,7 @@ public class ItemController {
                 modelMapperService.toObject(ItemDetailDto.class, itemService.updateItem(itemId, item))
         );
     }
-
+    @Operation(summary = "Operação para retornar um item(Serviço/Produto) por meio do ID.")
     @GetMapping("/{itemId}")
     public ResponseEntity<ItemDetailDto> getById(@PathVariable(name = "itemId") UUID itemId) {
         return ResponseEntity.ok(
@@ -47,12 +57,16 @@ public class ItemController {
         );
     }
 
+    @Operation(summary = "Operação para deletar um item(Serviço/Produto) por meio do ID.",
+            description = "Só é possível deletar um item caso não esteja em nenhum pedido.")
     @DeleteMapping("/{itemId}")
     public ResponseEntity<Void> deleteById(@PathVariable(name = "itemId") UUID itemId) {
         itemService.deleteItemById(itemId);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Operação para retornar uma página de items(Serviço/Produto).",
+            description = "Neste endpoint é possível aplicar os filtros e mudar paginação.")
     @GetMapping
     public ResponseEntity<PageResult<ItemDetailDto>> getAllItem(
             @RequestParam(required = false) UUID itemId,
--- src/main/java/com/senior/assessment/rest/OrderController.java
+++ src/main/java/com/senior/assessment/rest/OrderController.java
@@ -10,6 +10,8 @@ import com.senior.assessment.domain.enums.ItemType;
 import com.senior.assessment.domain.enums.OrderStatus;
 import com.senior.assessment.domain.querydsl.search.OrderSearch;
 import com.senior.assessment.domain.service.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -22,16 +24,28 @@ import static com.senior.assessment.utilities.Utils.createPagination;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("orders")
+@Tag(name = "Pedidos",
+        description = " Contém operações para Cadastrar, Listar, Editar e Excluir Pedidos e adicionar itens ao mesmo"
+)
 public class OrderController {
     private final OrderService orderService;
     private final ModelMapperService modelMapperService;
 
+    @Operation(summary = "Operação para criar um order(Pedido).",
+            description = "Olhe o schema OrderCreateUpdateDto para verificar as regras de cadastro. No cadastro de " +
+                    "produtos são adicionados os itens, então itens pedidos são criados a partir de  pedidos"
+    )
     @PostMapping
     public ResponseEntity<OrderDetailDto> create(@Valid @RequestBody OrderCreateUpdateDto orderCreate) {
         var order = orderService.createOrder(modelMapperService.toObject(Order.class, orderCreate));
         return ResponseEntity.ok(modelMapperService.toObject(OrderDetailDto.class, order));
     }
 
+    @Operation(summary = "Operação para atualizar um order(Pedido) por ID.",
+            description = "Olhe o schema OrderCreateUpdateDto para verificar as regras de cadastro. Pode ser " +
+                    "adicionados itens ou removidos. Só é possível editar um pedido, inclusive aplicar desconto caso " +
+                    "o pedido esteja em aberto(OPENED)."
+    )
     @PutMapping("/{orderId}")
     public ResponseEntity<OrderDetailDto> update(@PathVariable(name = "orderId") UUID orderId,
                                                  @Valid @RequestBody OrderCreateUpdateDto orderUpdate) {
@@ -41,6 +55,7 @@ public class OrderController {
         );
     }
 
+    @Operation(summary = "Operação para retornar um order(Pedido) por ID.")
     @GetMapping("/{orderId}")
     public ResponseEntity<OrderDetailDto> getById(@PathVariable(name = "orderId") UUID orderId) {
         return ResponseEntity.ok(
@@ -48,12 +63,18 @@ public class OrderController {
         );
     }
 
+    @Operation(summary = "Operação para deletar um order(Pedido) por ID.",
+            description = "Só é possível deletar um pedido caso seu status esteja em aberto, pedidos fechados não são" +
+                    " deletados."
+    )
     @DeleteMapping("/{orderId}")
     public ResponseEntity<Void> deleteById(@PathVariable(name = "orderId") UUID orderId) {
         orderService.deleteOrderById(orderId);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Operação para retornar uma página de orders(Pedidos).",
+            description = "Neste endpoint é possível aplicar os filtros e mudar paginação.")
     @GetMapping
     public ResponseEntity<PageResult<OrderDetailDto>> getAllOrder(
             @RequestParam(required = false) UUID orderId,
@@ -75,9 +96,11 @@ public class OrderController {
         return ResponseEntity.ok(modelMapperService.toPage(OrderDetailDto.class, result));
     }
 
+    @Operation(summary = "Operação para fechar(CLOSED) um pedido pedido por ID.",
+            description = "Neste endpoint é possível fechar um pedido caso esteja em aberto.")
     @PatchMapping("/{orderId}")
     public ResponseEntity<Void> updateStatusById(@PathVariable(name = "orderId") UUID orderId,
-                                           @Valid @RequestBody OrderStatusChangeDto orderStatusDto) {
+                                                 @Valid @RequestBody OrderStatusChangeDto orderStatusDto) {
         orderService.updateStatus(orderId, orderStatusDto.getStatus());
         return ResponseEntity.noContent().build();
     }
